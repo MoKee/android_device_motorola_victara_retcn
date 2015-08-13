77a130cf16217382799c3344d4781ba80be4ae73
martincz <martincz@mokeedev.com>
2015-08-14 01:43:54 +0800

    victara_retcn: fix set product model
    Change-Id: I907f6b1d2f9d807faa3553e78573b052c0fa3ed3

--- mokee.mk
+++ mokee.mk
@@ -8,6 +8,7 @@ $(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
 
 PRODUCT_RELEASE_NAME := MOTO X (2014)
 PRODUCT_NAME := mk_victara_retcn
+PRODUCT_MODEL := XT1085
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=victara_retcn \
