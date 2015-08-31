390857fcf0817409e97c0d60ffa43d212d8e583a
martincz <martincz@mokeedev.com>
2015-08-31 19:28:22 +0800

    tmp disable sabermod toolchain for test
    Change-Id: Iad0e79c775c1b4d6f25d041be2cb5a91ad513513

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,9 +26,6 @@ TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 # Assert
 TARGET_OTA_ASSERT_DEVICE := victara,victara_retcn
 
-# Use SaberMod Toolchains
-#SABERMOD_TOOLCHAIN_ENABLED := true
-
 # Platform
 TARGET_BOARD_PLATFORM := msm8974
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno330
