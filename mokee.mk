a03f8a4423ba72a3ff22b47d866ab8a45357e2e2
Chenxi Mao <chenxi.mao@sonymobile.com>
2016-02-10 19:57:11 +0900

    victara_retcn: fix overlay settings
    Change-Id: Id31b37fc5d95eb2b81752831d8c16da529455d7e

--- mokee.mk
+++ mokee.mk
@@ -6,11 +6,10 @@ $(call inherit-product, vendor/mk/config/common_full_phone.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
 
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
 PRODUCT_RELEASE_NAME := MOTO X (2014)
 PRODUCT_NAME := mk_victara_retcn
-PRODUCT_MODEL := XT1085
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=victara_retcn
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
